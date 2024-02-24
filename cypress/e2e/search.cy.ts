858cb9db05ce001b3a8bdc4112184e18c90b464a
Slav <kurochkinsalv@gmail.com>
2024-02-24 00:01:38 -0500

    test

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -31,8 +31,6 @@ describe('Search the tickets', () => {
       
       //h.search.getDeartureDateValue().should('have.attr', 'value', `${threeDaysFromNowFormated['formattedDateShort']}`)
       h.search.getReturnDateValue().should('have.attr', 'value', `${fiveDaysFromNowFormated['formattedDateShort']}`)
-      h.search.getReturnDateValue().should('have.attr', 'value', `${fiveDaysFromNowFormated['formattedDateShort']}`)
-      h.search.getReturnDateValue().should('have.attr', 'value', `${fiveDaysFromNowFormated['formattedDateShort']}`)
     })
   })
   
